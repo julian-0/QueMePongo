4796849f276298ce58007d407798cdcde77ef129
Matias Szlajen <mszlajen@est.frba.utn.edu.ar>
2019-04-23 16:32:29 -0300

    Define atuendo

--- src/main/java/Que_me_pongo/Atuendo.java
+++ src/main/java/Que_me_pongo/Atuendo.java
@@ -1,8 +1,33 @@
 package Que_me_pongo;
 
+import java.util.Objects;
+
 public class Atuendo {
-	Prenda superior;
-	Prenda inferior;
-	Prenda calzado;
-	Prenda accesorio;
+	private Prenda superior;
+	private Prenda inferior;
+	private Prenda calzado;
+	private Prenda accesorio;
+	
+	public Atuendo(Prenda superior, Prenda inferior, Prenda calzado, Prenda accesorio) {
+		this.superior = Objects.requireNonNull(superior);
+		this.inferior = Objects.requireNonNull(inferior);
+		this.calzado = Objects.requireNonNull(calzado);
+		this.accesorio = accesorio;
+	}
+	
+	public Prenda getSuperior() {
+		return this.superior;
+	}
+	
+	public Prenda getInferior() {
+		return this.inferior;
+	}
+	
+	public Prenda getCalzado() {
+		return this.calzado;
+	}
+	
+	public Prenda getAccesorio() {
+		return this.accesorio;
+	}
 }
