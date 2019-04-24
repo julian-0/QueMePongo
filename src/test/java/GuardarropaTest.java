819b1d83a362668b09b1868f1ba86e5e9c4b1a8d
Matias Szlajen <mszlajen@est.frba.utn.edu.ar>
2019-04-24 17:09:16 -0300

    Agregue test para la alternativa de atuendos

--- src/test/java/GuardarropaTest.java
+++ src/test/java/GuardarropaTest.java
@@ -1,8 +1,11 @@
 import Que_me_pongo.*;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.awt.*;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -15,6 +18,23 @@ public class GuardarropaTest {
 	Prenda accesorioA = PrendaFactory.crearAnteojos(Material.PLASTICO, Color.ORANGE, null);
 	Prenda zapatoA = PrendaFactory.crearZapatosDeTacon(Material.CUERO, Color.BLUE, null);
 	Prenda zapatoB = PrendaFactory.crearZapatosDeTacon(Material.CUERO, Color.GREEN, null);
+	
+	Atuendo atuendoA = new Atuendo(remera, pantalonA, zapatoA, accesorioA);
+	Atuendo atuendoB = new Atuendo(remera, pantalonA, zapatoB, accesorioA);
+	Atuendo atuendoC = new Atuendo(remera, pantalonA, zapatoA, PrendaFactory.noAccesorio());
+	Atuendo atuendoD = new Atuendo(remera, pantalonA, zapatoB, PrendaFactory.noAccesorio());
+	Atuendo atuendoE = new Atuendo(remera, pantalonB, zapatoA, accesorioA);
+	Atuendo atuendoF = new Atuendo(remera, pantalonB, zapatoB, accesorioA);
+	Atuendo atuendoG = new Atuendo(remera, pantalonB, zapatoA, PrendaFactory.noAccesorio());
+	Atuendo atuendoH = new Atuendo(remera, pantalonB, zapatoB, PrendaFactory.noAccesorio());
+	Atuendo atuendoI = new Atuendo(remeraB, pantalonA, zapatoA, accesorioA);
+	Atuendo atuendoJ = new Atuendo(remeraB, pantalonA, zapatoB, accesorioA);
+	Atuendo atuendoK = new Atuendo(remeraB, pantalonA, zapatoA, PrendaFactory.noAccesorio());
+	Atuendo atuendoL = new Atuendo(remeraB, pantalonA, zapatoB, PrendaFactory.noAccesorio());
+	Atuendo atuendoM = new Atuendo(remeraB, pantalonB, zapatoA, accesorioA);
+	Atuendo atuendoN = new Atuendo(remeraB, pantalonB, zapatoB, accesorioA);
+	Atuendo atuendoO = new Atuendo(remeraB, pantalonB, zapatoA, PrendaFactory.noAccesorio());
+	Atuendo atuendoP = new Atuendo(remeraB, pantalonB, zapatoB, PrendaFactory.noAccesorio());
 
 	@Test
 	public void cargaGuardarropaEnUsuario() {
@@ -39,6 +59,14 @@ public class GuardarropaTest {
 		guardarropa.agregarPrenda(remera);
 		Assert.assertEquals(1, guardarropa.cantidadPrendasEn(Categoria.SUPERIOR));
 	}
+	
+	@Rule
+	public ExpectedException expectedEx = ExpectedException.none();
+	@Test 
+	public void cargarPrendaConNullGeneraNullPointerException() throws Exception {
+		expectedEx.expect(NullPointerException.class);
+		(new Guardarropa()).agregarPrenda(null);
+	}
 
 	@Test
 	public void generaCorrectamenteLosAtuendos() {
@@ -51,40 +79,10 @@ public class GuardarropaTest {
 		guardarropa.agregarPrenda(zapatoA);
 		guardarropa.agregarPrenda(zapatoB);
 
-		Atuendo atuendoA = new Atuendo(remera, pantalonA, zapatoA, accesorioA);
-		Atuendo atuendoB = new Atuendo(remera, pantalonA, zapatoB, accesorioA);
-		Atuendo atuendoC = new Atuendo(remera, pantalonA, zapatoA, PrendaFactory.noAccesorio());
-		Atuendo atuendoD = new Atuendo(remera, pantalonA, zapatoB, PrendaFactory.noAccesorio());
-		Atuendo atuendoE = new Atuendo(remera, pantalonB, zapatoA, accesorioA);
-		Atuendo atuendoF = new Atuendo(remera, pantalonB, zapatoB, accesorioA);
-		Atuendo atuendoG = new Atuendo(remera, pantalonB, zapatoA, PrendaFactory.noAccesorio());
-		Atuendo atuendoH = new Atuendo(remera, pantalonB, zapatoB, PrendaFactory.noAccesorio());
-		Atuendo atuendoI = new Atuendo(remeraB, pantalonA, zapatoA, accesorioA);
-		Atuendo atuendoJ = new Atuendo(remeraB, pantalonA, zapatoB, accesorioA);
-		Atuendo atuendoK = new Atuendo(remeraB, pantalonA, zapatoA, PrendaFactory.noAccesorio());
-		Atuendo atuendoL = new Atuendo(remeraB, pantalonA, zapatoB, PrendaFactory.noAccesorio());
-		Atuendo atuendoM = new Atuendo(remeraB, pantalonB, zapatoA, accesorioA);
-		Atuendo atuendoN = new Atuendo(remeraB, pantalonB, zapatoB, accesorioA);
-		Atuendo atuendoO = new Atuendo(remeraB, pantalonB, zapatoA, PrendaFactory.noAccesorio());
-		Atuendo atuendoP = new Atuendo(remeraB, pantalonB, zapatoB, PrendaFactory.noAccesorio());
-
 		Set<Atuendo> setAtuendos = new HashSet<>();
-		setAtuendos.add(atuendoA);
-		setAtuendos.add(atuendoB);
-		setAtuendos.add(atuendoC);
-		setAtuendos.add(atuendoD);
-		setAtuendos.add(atuendoE);
-		setAtuendos.add(atuendoF);
-		setAtuendos.add(atuendoG);
-		setAtuendos.add(atuendoH);
-		setAtuendos.add(atuendoI);
-		setAtuendos.add(atuendoJ);
-		setAtuendos.add(atuendoK);
-		setAtuendos.add(atuendoL);
-		setAtuendos.add(atuendoM);
-		setAtuendos.add(atuendoN);
-		setAtuendos.add(atuendoO);
-		setAtuendos.add(atuendoP);
+		setAtuendos.addAll(Arrays.asList(atuendoA, atuendoB, atuendoC, atuendoD, 
+				atuendoE, atuendoF, atuendoG, atuendoH, atuendoI, atuendoJ, atuendoK,
+				atuendoL, atuendoM, atuendoN, atuendoO, atuendoP));
 		
 		Set<Atuendo> atuendos = guardarropa.atuendos();
 
@@ -101,6 +99,39 @@ public class GuardarropaTest {
 		guardarropa.agregarPrenda(pantalonA);
 		
 		Assert.assertTrue(guardarropa.atuendos().isEmpty());
+	}
+	
+	@Test
+	public void generaCorrectamenteLosAtuendosV2() {
+		Guardarropa guardarropa = new Guardarropa();
+		guardarropa.agregarPrenda(remera);
+		guardarropa.agregarPrenda(remeraB);
+		guardarropa.agregarPrenda(pantalonA);
+		guardarropa.agregarPrenda(pantalonB);
+		guardarropa.agregarPrenda(accesorioA);
+		guardarropa.agregarPrenda(zapatoA);
+		guardarropa.agregarPrenda(zapatoB);
+
+		Set<Atuendo> setAtuendos = new HashSet<>();
+		setAtuendos.addAll(Arrays.asList(atuendoA, atuendoB, atuendoC, atuendoD, 
+				atuendoE, atuendoF, atuendoG, atuendoH, atuendoI, atuendoJ, atuendoK,
+				atuendoL, atuendoM, atuendoN, atuendoO, atuendoP));
+		
+		Set<Atuendo> atuendos = guardarropa.atuendos2();
+
+		Assert.assertEquals(setAtuendos.size(), atuendos.size());
+		setAtuendos.forEach(atuendo -> Assert.assertTrue(atuendos.contains(atuendo)));
+	}
+	
+	@Test
+	public void sinNoTieneSuficientesPrendasGeneraUnaColeccionVaciaV2() {
+		Guardarropa guardarropa = new Guardarropa();
+		
+		guardarropa.agregarPrenda(remera);
+		guardarropa.agregarPrenda(remeraB);
+		guardarropa.agregarPrenda(pantalonA);
+		
+		Assert.assertTrue(guardarropa.atuendos2().isEmpty());
 	}
 
 }
