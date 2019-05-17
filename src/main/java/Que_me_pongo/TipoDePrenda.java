d9f5fdb0d17025ec9d32f1b05fc5c0405c778704
Matias Szlajen <mszlajen@est.frba.utn.edu.ar>
2019-05-17 17:30:50 -0300

    Validacion de nulos

--- src/main/java/Que_me_pongo/TipoDePrenda.java
+++ src/main/java/Que_me_pongo/TipoDePrenda.java
@@ -1,6 +1,7 @@
 package Que_me_pongo;
 
 import java.util.List;
+import java.util.Objects;
 
 public class TipoDePrenda {
 	private Tipo tipo;
@@ -8,9 +9,9 @@ public class TipoDePrenda {
 	private List<Material> materialesValidos;
 	
 	public TipoDePrenda(Tipo tipo, Categoria categoria, List<Material> materialesValidos) {
-		this.tipo = tipo;
-		this.categoria = categoria;
-		this.materialesValidos = materialesValidos;
+		this.tipo = Objects.requireNonNull(tipo);
+		this.categoria = Objects.requireNonNull(categoria);
+		this.materialesValidos = Objects.requireNonNull(materialesValidos);
 	}
 	
 	public Boolean validarMaterial(Material material) {
