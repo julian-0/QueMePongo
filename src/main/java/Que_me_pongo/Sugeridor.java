357fd7a868681243228d59785a3163562e191c9b
Matias Szlajen <mszlajen@est.frba.utn.edu.ar>
2019-05-29 10:28:19 -0300

    Logica para decidir

--- src/main/java/Que_me_pongo/Sugeridor.java
+++ src/main/java/Que_me_pongo/Sugeridor.java
@@ -4,15 +4,23 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import com.google.common.collect.Range;
+
 public class Sugeridor {
-	public Set<List<Prenda>> sugerir(Set<List<Prenda>> atuendos, int celsius) {
+	public Set<List<Prenda>> sugerir(Set<List<Prenda>> atuendos, double celsius) {
 		return atuendos.stream().
-					 filter(atuendo -> this.sugerirAtuendo(atuendo, celsius)).
+					 filter(atuendo -> this.sugerirAtuendo(atuendo, celsius, 10)).
 					 collect(Collectors.toSet());
 	}
 	
-	private boolean sugerirAtuendo(List<Prenda> atuendo, int celsius) {
-		return true;
+	private boolean sugerirAtuendo(List<Prenda> atuendo, double celsius, double margen) {
+		double nivelAbrigoTotal = atuendo.stream().reduce(0, this::reducirNivelAbrigo, (n1, n2) -> n1 + n2); 
+		return Range.open(celsius-margen, celsius+margen).contains(1 / nivelAbrigoTotal);
 	}
+	
+	private int reducirNivelAbrigo(int nivelPrenda1, Prenda prenda2) {
+		return nivelPrenda1 + prenda2.getNivelAbrigo();
+	}
+	
 
 }
