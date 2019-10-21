562ffdbb9fb8b8f9fec2c19ee89cac83e7b9fa46
Manuel Viegas <mviegas94@gmail.com>
2019-10-20 23:06:06 -0300

    agrego creacion de eventos sin funcionar

--- src/main/java/que_me_pongo/webApp/Router.java
+++ src/main/java/que_me_pongo/webApp/Router.java
@@ -12,15 +12,21 @@ public class Router {
 		LogInController loginController = new LogInController();
 		Spark.get("/login", loginController::show);
 		Spark.post("/login", loginController::create);
+
 		GuardarropasController guardarropasController = new GuardarropasController();
 		Spark.get("/guardarropas", guardarropasController::show);
 		Spark.get("/guardarropas/:id", guardarropasController::listarPrendas);
+
 		EventosController eventosController = new EventosController();
 		Spark.get("/eventos", eventosController::index);
+		Spark.post("/evento", eventosController::create);
+		Spark.get("/evento/nuevo", eventosController::nuevo);
 		Spark.get("/evento/:id", eventosController::show);
 		Spark.get("/eventosJson", eventosController::entradasCalendario);
+
 		SugerenciasController sugerenciasController = new SugerenciasController();
 		Spark.get("/evento/:id/sugerencias", sugerenciasController::show);
 		Spark.post("/evento/:id/sugerencias", sugerenciasController::modificar);
+
 	}
 }
