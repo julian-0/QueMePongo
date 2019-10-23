bff34cdcaa8f9f59b8dc568006bc6139f0882eda
Matias Szlajen <mszlajen@est.frba.utn.edu.ar>
2019-10-23 16:36:06 -0300

    Agrego inicio de ejecución de Job

--- src/main/java/que_me_pongo/webApp/WebAppMain.java
+++ src/main/java/que_me_pongo/webApp/WebAppMain.java
@@ -1,4 +1,7 @@
 package que_me_pongo.webApp;
+import org.quartz.SchedulerException;
+
+import que_me_pongo.EventoMain;
 import que_me_pongo.ExampleDataCreator;
 import que_me_pongo.usuario.RepositorioUsuarios;
 import que_me_pongo.usuario.TipoUsuario;
@@ -8,10 +11,11 @@ import spark.debug.DebugScreen;
 
 public class WebAppMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SchedulerException  {
 		Spark.port(9000);
 		DebugScreen.enableDebugScreen();
 		Router.configurar();
 		new DB().configurarTransacciones();
+		EventoMain.main(args);
 	}
 }
